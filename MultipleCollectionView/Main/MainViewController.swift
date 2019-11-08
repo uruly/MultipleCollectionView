97b2f40baf59d895eb91d1d274f33692f6b125d9
uruly <rk.uru007@gmail.com>
2019-11-08 22:52:09 +0900

    :rocket: Initialize setting

---
+++ MultipleCollectionView/Main/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  MultipleCollectionView
+//
+//  Created by Reona Kubo on 2019/11/08.
+//  Copyright © 2019 Reona Kubo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
